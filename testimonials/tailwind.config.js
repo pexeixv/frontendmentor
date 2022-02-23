9f7f6a8661751e129daec3218b99f7de4e595479
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-02-23 16:33:55 +0530

    tailwind cdn removed and minified

---
+++ testimonials/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    'index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violet: 'hsl(263, 55%, 52%)',
+        darkgrayishblue: 'hsl(217, 19%, 35%)',
+        blackishblue: 'hsl(219, 29%, 14%)',
+        white: 'hsl(0, 0%, 100%)',
+        gray: 'hsl(0, 0%, 81%)',
+        lightgrayishblue: 'hsl(210, 46%, 95%)'
+      },
+      fontFamily: {
+        'barlow': ['Barlow Semi Condensed', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
