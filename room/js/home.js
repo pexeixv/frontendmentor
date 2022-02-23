fc9b671885c2d0944cbf2c637d5d15c5a1015f97
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-02-23 20:07:01 +0530

    idk

--- room/js/home.js
+++ room/js/home.js
@@ -1,5 +1,5 @@
-var defaultPath = '../img/';
-var slides = [
+let defaultPath = 'img/'
+let slides = [
     {
         "title": 'Discover innovative ways to decorate',
         "desc": "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
@@ -15,19 +15,17 @@ var slides = [
         "desc": "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
         "image": 'desktop-image-hero-3.jpg'
     }
-];
+]
 
 
-
-
-
-var counter = 0;
-var header = document.querySelector('header');
-var h1 = document.querySelector('main h1');
-var p = document.querySelector('main p');
-var a = document.querySelector('main > a');
+let counter = 0;
+let header = document.querySelector('header')
+let h1 = document.querySelector('main h1')
+let p = document.querySelector('main p')
+let a = document.querySelector('main > a')
 
 function change(con) {
+    counter += 6
     if (con == 'next')
         counter++;
     else if (con == 'prev')
@@ -35,61 +33,52 @@ function change(con) {
     n = counter % 3;
     if (n < 0)
         n = -n;
-    header.style.background = 'url(' + defaultPath + slides[n].image + ') no-repeat center center/cover';
+    header.style.background = 'url(' + defaultPath + slides[n].image + ') no-repeat center center/cover'
 
-    setTimeout(function () {
-        a.style.opacity = 0;
+    setTimeout(() => {
+        p.style.opacity = 0
+        a.style.opacity = 0
+        h1.style.opacity = 0
     }, 200);
-    setTimeout(function () {
-        p.style.opacity = 0;
-    }, 400);
-    setTimeout(function () {
-        h1.style.opacity = 0;
-    }, 600);
 
-    setTimeout(function () {
-        h1.innerHTML = slides[n].title;
-        h1.style.opacity = 1;
-    }, 1000);
-    setTimeout(function () {
-        p.innerHTML = slides[n].desc;
-        p.style.opacity = 1;
-    }, 1200);
-    setTimeout(function () {
-        a.style.opacity = 1;
-    }, 1400);
+    setTimeout(() => {
+        h1.innerHTML = slides[n].title
+        h1.style.opacity = 1
+    }, 500);
+    setTimeout(() => {
+        p.innerHTML = slides[n].desc
+        p.style.opacity = 1
+    }, 700)
+    setTimeout(() => {
+        a.style.opacity = 1
+    }, 900)
 }
 
 
 
 
-ham = document.querySelector('.hamburger');
-nav = document.querySelector('nav');
-bars = document.querySelectorAll('.hamburger .bar');
-bar1 = document.querySelector('.hamburger .bar1');
-bar2 = document.querySelector('.hamburger .bar2');
-bar3 = document.querySelector('.hamburger .bar3');
-
+let ham = document.querySelector('.hamburger')
+let nav = document.querySelector('nav')
+let bars = document.querySelectorAll('.hamburger .bar')
 
 ham.addEventListener('click', () => {
-    cond = nav.classList.toggle('show');
+    const cond = nav.classList.toggle('show')
     bars.forEach(bar => {
         if (cond)
-            bar.style.background = 'gray';
-        else bar.style.background = 'white';
+            bar.style.background = 'gray'
+        else bar.style.background = 'white'
     })
     if (cond) {
         // ham.style.transform = 'translateX(-10px)';
-        bar2.style.opacity = '0';
-        bar3.style.transform = 'rotate(-45deg) translate(10px,-4.5px)';
-        bar1.style.transform = 'rotate(45deg) translate(10px,4.5px)';
+        bars[2 - 1].style.opacity = '0';
+        bars[3 - 1].style.transform = 'rotate(-45deg) translate(10px,-4.5px)'
+        bars[1 - 1].style.transform = 'rotate(45deg) translate(10px,4.5px)'
 
     } else {
-        bar2.style.opacity = '1';
-        bar3.style.transform = 'none';
-        bar1.style.transform = 'none';
+        bars[2 - 1].style.opacity = '1';
+        bars[3 - 1].style.transform = 'none';
+        bars[1 - 1].style.transform = 'none';
     }
 })
-
 
 
